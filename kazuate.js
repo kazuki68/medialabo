86aa43f3b6a4b77ef9c33c6e0f3756eb76502f41
G386622023 <238662@st.takushoku-u.ac.jp>
2024-06-25 23:34:43 +0900

    3回目課題

--- kazuate.js
+++ kazuate.js
@@ -16,9 +16,24 @@ hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
+  kaisu = kaisu + 1;
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+  console.log(kaisu + '回目の予想:'+yoso);
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
+  if(4 <= kaisu){
+    console.log('答えは'+kotae+'でした。すでにゲームは終わっています。');
+  }
+  if(kaisu < 4 && kotae === yoso){
+    console.log('正解です。おめでとう');
+    kaisu = kaisu + 3;
+  }
+  if(kaisu < 4 && kotae < yoso){
+    console.log('間違い、もう少し小さいですよ');
+  }
+  if(kaisu < 4 && kotae > yoso){
+    console.log('間違い、もう少し大きいですよ');
+ }
 }
