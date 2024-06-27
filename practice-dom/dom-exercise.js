ebfe4b58eebb525771745fd7be8f04f03e94f776
G386622023 <238662@st.takushoku-u.ac.jp>
2024-06-27 14:57:21 +0900

    4回

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -14,6 +14,10 @@ let data = [
 //// 注意: 以上は編集しないこと!
 
 // 練習4-2 メッセージ追加プログラム
+let h1 = document.querySelector('h2#ex42');
+let p = document.createElement('p');
+p.textContent = '写真表と都市の緯度経度のページです' ;
+
 
 
 // 練習4-3 写真表作成プログラム
